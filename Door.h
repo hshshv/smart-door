eef7126421056c91c8927a94a1b37a2ab08e7d33
hshshv <53831820+hshshv@users.noreply.github.com>
2020-01-28 15:53:39 +0200

    Update Door.h

--- Door.h
+++ Door.h
@@ -4,13 +4,88 @@
 #include "Arduino.h"
 #include "locker.h"
 #include "RGBLed.h"
+#include "UltrasonicSensor.h"
+#include "MaxMatrix.h"
 
 class Door
 {
   private:
     RGBLed Led;
+    UltrasonicSensor ExternalSensor;
+    UltrasonicSensor InternalSensor;
+    byte Buzzer;
+    byte maxInUse = 1;
+    MaxMatrix matrix;
+    static byte Sprites[6][10] = {{
+        8, 8,
+        B00000000,
+        B00000000,
+        B00000000,
+        B00000000,
+        B00000000,
+        B00000000,
+        B00000000,
+        B00000000
+      }, {
+        8, 8,
+        B00000000,
+        B00000000,
+        B00000000,
+        B00010000,
+        B00000000,
+        B00000000,
+        B00000000,
+        B00000000
+      }, {
+        8, 8,
+        B00000000,
+        B00000000,
+        B00000000,
+        B00011000,
+        B00011000,
+        B00000000,
+        B00000000,
+        B00000000
+      }, {
+        8, 8,
+        B00000000,
+        B00000000,
+        B00111100,
+        B00111100,
+        B00111100,
+        B00111100,
+        B00000000,
+        B00000000
+      }, {
+        8, 8,
+        B00000000,
+        B01111110,
+        B01111110,
+        B01111110,
+        B01111110,
+        B01111110,
+        B01111110,
+        B00000000
+      }, {
+        8, 8,
+        B11111111,
+        B11111111,
+        B11111111,
+        B11111111,
+        B11111111,
+        B11111111,
+        B11111111,
+        B11111111
+      }
+    };
+    bool SomeoneIsComing();
+    bool BotheSidesAreBlocked();
+    float GetLowerDistance();
+    void RefreshMatrix();
+    unsigned long CloseAt;
+
   public:
-    Door(int HingePin, int LockerPin, int RedPin, int GreenPin);
+    Door(int HingePin, int LockerPin, int RedPin, int GreenPin, int InSensorTrig, int InSensorEcho, int ExSensorTrig, int ExSensorEcho, int Din, int Cs, int Clk, int BuzzerPin);
     locker Locker;
     locker Hinge;
     void Open();
@@ -19,5 +94,7 @@ class Door
     void UnLock();
     bool IsLock();
     bool IsOpen();
+    void Refresh();
+
 };
 #endif
